b843e857b6b380c4a95d1e418eb730e578f2934e
renatagomesdeandrade <133373733+renatagomesdeandrade@users.noreply.github.com>
2024-10-02 22:09:50 -0300

    Update JOIN - Biblioteca.sql

--- JOIN - Biblioteca.sql
+++ JOIN - Biblioteca.sql
@@ -7,19 +7,37 @@ SELECT nome, data_emprestimo, data_devolucao, titulo FROM membros AS M
 INNER JOIN 	emprestimos AS E ON M.id_membro = E.id_membro
 INNER JOIN livros AS L ON E.id_livro = L.id_livro;
 
-SELECT * FROM tb_emprestimo;
+SELECT * FROM tb_emprestimos;
 
-
--- RIGHT JOIN
+-- RIGHT / LEFT JOIN
 -- Exercício 1: Liste todos os membros, incluindo aqueles que não realizaram nenhum empréstimo.
--- Exercício 2: Liste todos os autores e seus livros, incluindo livros que não foram emprestados.
+-- RIGHT JOIN
+SELECT * FROM emprestimos AS E 
+RIGHT JOIN membros AS M
+ON M.id_membros = E.id_membro
+WHERE E.id_emprestimo IS NULL;
+
+-- LEFT JOIN
+SELECT * FROM membros AS M
+LEFT JOIN emprestimos AS E
+ON M.id_membro = E.id_membro
+WHERE E.id_emprestimo IS NULL;
+
 SELECT nome, titulo, ano_publicacao, data_emprestimo FROM autores AS A
 INNER JOIN livros AS L ON A.id_autor = L.id_autor
 INNER JOIN emprestimos AS E ON L.id_livro = E.id_livro;
 
+-- Exercício 2: Liste todos os autores e seus livros, incluindo livros que não foram emprestados.
+SELECT * FROM livros AS L
+LEFT JOIN emprestimos AS E ON L.id_emprestimo = E.id_emprestimo
+INNER JOIN autores AS A ON L.id_autor = A.id_autor
+WHERE E.id IS NULL;
+
+
 -- UNION
 -- Exercício 1: Liste todos os livros e seus autores, incluindo livros que não têm
 -- autores associados e autores que não têm livros cadastrados.
+
+
 -- Exercício 2: Liste todos os membros e livros emprestados, incluindo membros
 -- que não pegaram livros e livros que não foram emprestados.
-
